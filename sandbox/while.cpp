b032fef87298cede6cbd44657da6e533a70dc771
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-08 12:46:09 -0400

    update version 1.0.1

---
+++ sandbox/while.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+        
+    int a = 5; 
+    int b = 10;
+    int c = a + b; 
+    cout << "Hello World" << endl;
+    cout << "The sum of a and b is " << c << endl;
+    while (a <= b)
+    {
+        cout << "a is " << a << endl;
+        a++;
+    }
+    {
+        /* code */
+    }
+    {
+        /* code */
+    }
+    
+}
