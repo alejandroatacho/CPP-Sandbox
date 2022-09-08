e035f24385e150fc45aecd40e930ba5d978772b8
Hinamizawa <hinamizawa.github@gmail.com>
2022-09-08 11:46:42 -0400

    update

--- sandbox/tutorial1.cpp
+++ sandbox/tutorial1.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
-
+ 
 int main()
 {
+        
+    int a = 5; 
+    int b = 10;
+    int c = a + b; 
     cout << "Hello World" << endl;
+    cout << "The sum of a and b is " << c << endl;
     return 0;
 }
