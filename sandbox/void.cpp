efe5c27237ef13d1b837b1dc5a8d78eba88bb4e7
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-15 19:29:59 -0400

    update

---
+++ sandbox/void.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+void printMessage()
+{
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "I'm a function!" << std::endl;
+}
+
+int main()
+{
+    printMessage();
+    return 0;
+}
