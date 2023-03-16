2c3e499aa6892c103ed74084922148ae98b0e342
Hinamizawa <hinamizawa.github@gmail.com>
2023-03-15 20:14:17 -0400

    Update move.sh

--- move.sh
+++ move.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
 cd sandbox
+
+if [ -z "$(ls -A *.exe)" ]; then
+echo "No .exe files found."
+else
 mv *.exe output/
+fi
+
+if [ -z "$(ls -A *.dll)" ]; then
+echo "No .dll files found."
+else
 mv *.dll output/
+fi
